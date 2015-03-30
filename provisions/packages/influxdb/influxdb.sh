5864a646452d0fd4f2eb16952fcf5e944d4c6858
Gilles De Mey <gilles.de.mey@gmail.com>
2015-03-31 01:03:57 +0200

    automatically create database

--- provisions/packages/influxdb/influxdb.sh
+++ provisions/packages/influxdb/influxdb.sh
@@ -3,3 +3,7 @@
 wget http://get.influxdb.org/influxdb_0.9.0-rc17_amd64.deb
 dpkg -i influxdb_0.9.0-rc17_amd64.deb
 service influxdb restart
+
+sleep 2
+echo "creating database"
+curl -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE test"

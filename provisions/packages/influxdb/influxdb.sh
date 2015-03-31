551b8ac4eee83905e17ea00fb9a3644af5debf04
Gilles De Mey <gilles.de.mey@gmail.com>
2015-04-01 00:37:36 +0200

    Sleep longer for creating db

--- provisions/packages/influxdb/influxdb.sh
+++ provisions/packages/influxdb/influxdb.sh
@@ -4,6 +4,6 @@ wget http://get.influxdb.org/influxdb_0.9.0-rc17_amd64.deb
 dpkg -i influxdb_0.9.0-rc17_amd64.deb
 service influxdb restart
 
-sleep 2
-echo "creating database"
+sleep 10
+echo "creating database..."
 curl -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE test"

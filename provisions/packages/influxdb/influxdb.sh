8623f69b862874cd95a926b3bca46d9a5aa10ff9
Gilles De Mey <gilles.de.mey@gmail.com>
2015-04-01 03:14:10 +0200

    No point in sleeping longer it seems

--- provisions/packages/influxdb/influxdb.sh
+++ provisions/packages/influxdb/influxdb.sh
@@ -4,6 +4,6 @@ wget http://get.influxdb.org/influxdb_0.9.0-rc17_amd64.deb
 dpkg -i influxdb_0.9.0-rc17_amd64.deb
 service influxdb restart
 
-sleep 10
+sleep 2
 echo "creating database..."
-curl -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE test"
+curl -G http://127.0.0.1:8086/query --data-urlencode "q=CREATE DATABASE test"
